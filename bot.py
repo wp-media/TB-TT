ff25a7c2ef8a823a140552185d69884730622e4c
Mathieu Lamiot <mathieu.lamiot@free.fr>
2023-07-23 12:42:47 +0200

    Added shortcut and modal response

--- bot.py
+++ bot.py
@@ -45,6 +45,81 @@ def event_hook(received_request):
 def interaction_hook():
     print(request.headers)
     print(request.form)
+    payload_json = json.loads(request.form.get('payload'))
+
+    interaction_type = payload_json['type']
+
+    if 'view_submission' == interaction_type:
+        print('yeah')
+        return {"response_action": "clear"}
+    elif 'shortcut' == interaction_type:
+        callback = payload_json['callback_id']
+        if 'ttl_create_github_task_general_shortcut' == callback:
+            print('create github task')
+            view = '''{
+                    "type": "modal",
+                    "title": {
+                        "type": "plain_text",
+                        "text": "Create a Github task"
+                    },
+                    "blocks": [
+                        {
+                            "type": "input",
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Title of the task"
+                            },
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "task_title",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "Type in here"
+                                },
+                                "multiline": false
+                            },
+                            "optional": false
+                        },
+                        {
+                            "type": "input",
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Description of the task"
+                            },
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "task_title",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "Describe the task, or provide a Slack thread link."
+                                },
+                                "multiline": true
+                            },
+                            "optional": false
+                        }
+                    ],
+                    "close": {
+                        "type": "plain_text",
+                        "text": "Cancel"
+                    },
+                    "submit": {
+                        "type": "plain_text",
+                        "text": "Save"
+                    },
+                    "private_metadata": "",
+                    "callback_id": "ttl_create_github_task_modal_submit"
+                    }'''
+
+            request_open_view_payload = dict()
+            request_open_view_header = {"Authorization": "Bearer " + os.environ['SLACK_BOT_TOKEN']}
+            request_open_view_payload['view'] = view
+            request_open_view_payload['trigger_id'] = payload_json['trigger_id']
+            response = requests.post(url='https://slack.com/api/views.open', headers=request_open_view_header, json=request_open_view_payload, timeout=3000)
+            print(response)
+        else:
+            return Response(status=501)
+    else:
+        return Response(status=501)
     return Response(status=200)
 
 # An example of one of your Flask app's routes
