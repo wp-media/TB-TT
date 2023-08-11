6ee788fd513cfbc05cff30c8e03bfe605f5e1fa6
Mathieu Lamiot <mathieu.lamiot@free.fr>
2023-08-11 09:35:46 +0200

    Add view specification in the Github URL when creating a task Updated Slack config for WP Media project

--- sources/handlers/GithubTaskHandler.py
+++ sources/handlers/GithubTaskHandler.py
@@ -1,7 +1,8 @@
 """
     This module defines the handler for GitHub task (ProjectV2Item) related logic.
 """
-
+import json
+from pathlib import Path
 from sources.factories.GithubGQLCallFactory import GithubGQLCallFactory
 from sources.factories.SlackMessageFactory import SlackMessageFactory
 from sources.models.InitGithubTaskParam import InitGithubTaskParam
@@ -18,12 +19,26 @@ class GithubTaskHandler():
         """
         self.github_gql_call_factory = GithubGQLCallFactory()
         self.slack_message_factory = SlackMessageFactory()
+        with open(Path(__file__).parent.parent.parent / "config" / "github.json", encoding='utf-8') as file_github_config:
+            self.github_config = json.load(file_github_config)
 
-    def get_task_link(self, project_number, item_number):
+    def get_task_link(self, project_number, view_number, item_number):
         """
             Generates the URL to display a Github task of WP Media from the project number and the project item number
         """
-        return f"https://github.com/orgs/wp-media/projects/{project_number}?pane=issue&itemId={item_number}"
+        # pylint: disable-next=line-too-long
+        return f"https://github.com/orgs/wp-media/projects/{project_number}/views/{view_number}?pane=issue&itemId={item_number}"
+
+    def get_board_view(self, flow):
+        """
+            Returns the board view associated to a given flow.
+        """
+        view_number = self.github_config["board_views"]["default"]
+
+        if flow is not None and flow in self.github_config["board_views"]:
+            view_number = self.github_config["board_views"][flow]
+
+        return view_number
 
     def init_github_task(self, app_context, task_params: InitGithubTaskParam):
         """
@@ -60,7 +75,10 @@ class GithubTaskHandler():
             # Send notifications to Slack
             if task_params.initiator is not None:
                 text = "You created a Github task: " + self.get_task_link(
-                    project_item.project_number, project_item.item_database_id)
+                    project_item.project_number,
+                    self.get_board_view(task_params.flow),
+                    project_item.item_database_id
+                    )
                 self.slack_message_factory.post_message(app_context, task_params.initiator, text)
 
             if 'dev-team-escalation' == task_params.flow:
@@ -69,7 +87,9 @@ class GithubTaskHandler():
 
                 # Send message on Slack channel
                 text = f"{task_params.title} by <@{task_params.initiator}>: " + self.get_task_link(
-                    project_item.project_number, project_item.item_database_id)
+                    project_item.project_number,
+                    self.get_board_view(task_params.flow),
+                    project_item.item_database_id)
                 self.slack_message_factory.post_message(app_context,
                                                         self.slack_message_factory.get_channel(task_params.flow),
                                                         text)
