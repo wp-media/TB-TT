50face3b75968c4d9e37ed8879fc241376ce9e33
Mathieu Lamiot <mathieu.lamiot@free.fr>
2024-05-28 09:11:20 +0200

    Update IPs with k8spod and latest changes

--- sources/handlers/ServerListHandler.py
+++ sources/handlers/ServerListHandler.py
@@ -23,9 +23,9 @@ class ServerListHandler():
             Lists all IP used by live1 cluster from group.One
         """
         live1_ips = ''
-        # Defined in k8s_sips:
+        # Defined in k8s_live1_sips & k8s_live1_ingress:
         # https://gitlab.one.com/systems/chef-repo/-/blob/master/roles/onecom-global-firewall-macros.json#L173
-        live1_ips += "46.30.212.67\n"
+        live1_ips += "46.30.212.67\n46.30.212.92\n"
         return live1_ips
 
     def get_groupone_live2_ipv4(self):
@@ -33,11 +33,11 @@ class ServerListHandler():
             Lists all IP used by live2 cluster from group.One
         """
         live2_ips = ''
-        # Defined in k8s_sips:
+        # Defined in k8s_live2_sips:
         # https://gitlab.one.com/systems/chef-repo/-/blob/master/roles/onecom-global-firewall-macros.json#L173
-        live2_ips += "46.30.212.64\n46.30.212.65\n46.30.212.66\n46.30.212.67\n46.30.212.68\n46.30.212.69\n46.30.211.85\n"
-        # Added for live2 according to JIRA: https://group-one.atlassian.net/browse/NET-283
-        live2_ips += "46.30.212.70\n46.30.212.71\n46.30.212.72\n46.30.212.73\n"
+        live2_ips += "46.30.212.68\n46.30.212.70\n46.30.212.71\n46.30.212.72\n46.30.212.73\n"
+        # Defined in k8s_live2_ingress:
+        live2_ips += "46.30.212.116\n"
         return live2_ips
 
     def get_groupone_k8spod1_ipv4(self):
@@ -45,9 +45,9 @@ class ServerListHandler():
             Lists all IP used by k8spod1 cluster from group.One
         """
         k8spod1_ips = ''
-        # Defined in k8s_sips:
+        # Defined in k8spod1_sips & k8spod1_ingress:
         # https://gitlab.one.com/systems/chef-repo/-/blob/master/roles/onecom-global-firewall-macros.json#L173
-        k8spod1_ips += "46.30.211.168\n"
+        k8spod1_ips += "46.30.211.168\n46.30.212.120\n"
         return k8spod1_ips
 
     def get_groupone_cpcss_ipv4(self):
@@ -63,7 +63,7 @@ class ServerListHandler():
             Lists all IPv4 used specifically by WP Rocket SaaS service from group.One
         """
         ipv4 = ''
-        ipv4 += "46.30.212.106\n"
+        ipv4 += "46.30.212.116\n"
         return ipv4
 
     def get_groupone_backend_ipv4(self):
@@ -98,13 +98,11 @@ class ServerListHandler():
         text += "Load CSS Asynchronously:\n"
         # Defined in https://gitlab.one.com/systems/group.one-authdns/-/blob/main/octodns/wp-rocket.me.yaml?ref_type=heads
         text += "https://cpcss.wp-rocket.me\n"
-        text += self.get_groupone_cpcss_ipv4()
         text += self.get_groupone_live2_ipv4()
         text += "\n"
 
         text += "Remove Unused CSS:\n"
         # SaaS CNAME in https://gitlab.one.com/systems/group.one-authdns/-/blob/main/octodns/wp-rocket.me.yaml?ref_type=heads
-        text += self.get_groupone_saas_ipv4()
         text += self.get_groupone_live2_ipv4()
         text += self.get_groupone_live1_ipv4()
         text += self.get_groupone_k8spod1_ipv4()
@@ -150,9 +148,6 @@ class ServerListHandler():
         text = ""
         # group.One
         text += self.get_groupone_proxy_ipv4()
-        text += self.get_groupone_cpcss_ipv4()
-        text += self.get_groupone_saas_ipv4()
-        text += self.get_groupone_backend_ipv4()
         text += self.get_groupone_live2_ipv4()
         text += self.get_groupone_live1_ipv4()
         text += self.get_groupone_k8spod1_ipv4()
